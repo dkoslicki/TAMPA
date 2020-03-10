180bcef85cffc9c1bfea41480dddd6ebe700f4fb
dkoslicki <david.koslicki@math.oregonstate.edu>
2020-03-10 11:35:40 +0100

    complete command line arguments

--- src/profile_to_plot.py
+++ src/profile_to_plot.py
@@ -8,12 +8,21 @@ ncbi = NCBITaxa()
 
 def main():
     argparser = argparse.ArgumentParser(description='Plot abundance of profile against ground truth on taxonomic tree.')
+    argparser.add_argument('-i', '--input_profile', type=str, help='Input taxonoomic profile')
+    argparser.add_argument('-g', '--ground_truth_input_profile', type=str, help='Input ground truth taxonoomic profile')
+    argparser.add_argument('-o', '--output_file', type=str, help='File for output (.svg, png, pdf, etc.')
+    argparser.add_argument('-s', '--sample_of_interest', type=str, help="If you're only interested in a single sample of interest, specify here.")
     argparser.add_argument('taxonomic_rank', type=str, help='Taxonomic rank to do the plotting at')
     params = argparser.parse_args()
     rank = params.taxonomic_rank
-    input_file = "/home/dkoslicki/Data/CAMI2/meta_coder_analysis/profiles/marine_short/MS15.profile"
-    ground_truth = "/home/dkoslicki/Data/CAMI2/meta_coder_analysis/profiles/marine_short/gs_marine_short.profile"
-    sample_of_interest = 'marmgCAMI2_short_read_sample_0'
+    #input_file = "/home/dkoslicki/Data/CAMI2/meta_coder_analysis/profiles/marine_short/MS15.profile"
+    input_file = params.input_profile
+    #ground_truth = "/home/dkoslicki/Data/CAMI2/meta_coder_analysis/profiles/marine_short/gs_marine_short.profile"
+    ground_truth = params.ground_truth_input_profile
+    #sample_of_interest = 'marmgCAMI2_short_read_sample_0'
+    sample_of_interest = params.sample_of_interest
+    output_file = params.output_file
+
     PF = ProfilesLayout(input_file, ground_truth, sample_of_interest=sample_of_interest, normalize=True)
 
     tree = ncbi.get_topology(PF.get_all_tax_ids(), rank_limit=rank)
@@ -34,7 +43,7 @@ def main():
     ts.legend.add_face(T, column=0)
     ts.optimal_scale_level = 'full'
     #tree.show(tree_style=ts)
-    tree.render('out.svg', tree_style=ts, h=5, w=5, units="in")
+    tree.render(output_file, h=5, w=5, tree_style=ts, units="in")
     #tree.render('out.svg', tree_style=ts)
 
 
